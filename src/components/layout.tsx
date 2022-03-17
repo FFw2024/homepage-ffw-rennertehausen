2d2ff6b6aa5bd2cc4025b79a95c91ef681222d0c
Philipp Geil <Philipp-Geil@outlook.com>
2022-03-17 21:18:09 +0100

    Add content adn set icon

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,9 +1,16 @@
+import Head from 'next/head';
+
 import Footer from "./footer";
 import Navbar from "./navbar";
 
 export default function Layout({ children }) {
     return (
         <>
+            <Head>
+                <title>Feuerwehr Rennertehausen</title>
+                {/* <meta name="description" content="" /> */}
+                <link rel="icon" href="/logo.png" />
+            </Head>
             <Navbar />
             <main className="container pt-3">
                 {children}
