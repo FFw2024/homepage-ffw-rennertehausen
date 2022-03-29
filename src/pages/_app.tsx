8bbb7aaaa12319db0c44922944937a6ed3629e43
Philipp Geil <Philipp-Geil@outlook.com>
2022-03-30 00:26:35 +0200

    Fix deployment errors

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,7 +8,7 @@ import '../styles/custom.scss'
 
 function MyApp({ Component, pageProps }) {
   return (<>
-    <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></Script>
+    <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossOrigin="anonymous"></Script>
     <Layout>
       <Component {...pageProps} />
     </Layout>
