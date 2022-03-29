16b121e8be954b7880205ec9d7432a15613bf676
Philipp Geil <Philipp-Geil@outlook.com>
2022-03-29 23:26:45 +0200

    prepare gh-pages deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: isProd ? 'https://github.com/PhiGei2000/homepage-ffw-rennertehausen/' : ''
 }
 
 module.exports = nextConfig
