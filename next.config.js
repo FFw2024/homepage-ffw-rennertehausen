568e1aaa71a88cdc956179f5083bbebd2e731f68
Philipp Geil <Philipp-Geil@outlook.com>
2022-03-30 13:17:36 +0200

    Remove deployment action

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production'
-
 const nextConfig = {
-  reactStrictMode: true,
-  assetPrefix: isProd ? 'https://github.com/PhiGei2000/homepage-ffw-rennertehausen/' : ''
+  reactStrictMode: true
 }
 
 module.exports = nextConfig
