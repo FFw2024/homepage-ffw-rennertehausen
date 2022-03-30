c044015a77f600cc3a7a797bebff1c37fb791667
Philipp Geil <Philipp-Geil@outlook.com>
2022-03-30 16:02:31 +0200

    Fix page bottom margin on mobile devices

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -7,15 +7,15 @@ export default class Footer extends Component {
             <footer className="fixed-bottom bg-dark container-fluid">
                 <div className="container my-4">
                     <div className="row">
-                        <div className="col-md-auto col-12 me-md-2">
+                        <div className="col-sm-auto col-12 me-sm-2">
                             <span className="text-muted">&copy; 2022 - Feuerwehr Rennertehausen</span>
                         </div>
-                        <div className="col-md-auto col-6 me-md-2">
+                        <div className="col-sm-auto col-6 me-sm-2">
                             <Link href="/impressum">
                                 <a className="text-primary text-decoration-none">Impressum</a>
                             </Link>
                         </div>
-                        <div className="col-md col-6">
+                        <div className="col-sm col-6">
                             <div className="d-flex justify-content-end">
                                 <Link href="https://www.instagram.com/feuerwehr_rth/">
                                     <a className="me-2" title="Instagram" target="_blank" rel="noreferrer">
