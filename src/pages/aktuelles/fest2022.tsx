eddc089c5bc92cdf61e108d47b41ed15a542430b
PhiGei2000 <Philipp-Geil@outlook.com>
2022-06-06 22:43:25 +0200

    Fix merge conflicts on page 'Fest 2022'

--- src/pages/aktuelles/fest2022.tsx
+++ src/pages/aktuelles/fest2022.tsx
@@ -1,19 +1,21 @@
 export default function fest2022() {
-    return (<>
-        <h1>Heimat- und Feuerwehrfest 2022</h1>
-        <p>
-            In diesem Jahr ist es endlich wieder soweit! Das traditionelle Heimat- und Feuerwehrfest in Rennertehausen wird nach 3 Jahren mit Einschränkungen durch die Corona-Pandemie wieder stattfinden.<br/>
-            Start ist am Freitag den 01. Juli 2022 ab 19 Uhr mit einem 11m-Turnier. Dabei gibt es für die ersten drei Plätze Geldpreise zu gewinnen. Die Endrundenteilnehmer erwarten verschiedene Sachpreise. Anmelden könnt ihr euch über die ausliegenden Flyer im Festzelt sowie über unsere Facebookseite oder per Mail an <span><a href="mailto://vorstand@feuerwehr-rennertehausen.de" className="text-decoration-none">vorstand@feuerwehr-rennertehausen.de</a></span> erfolgen. Dabei bitte einen Teamnamen und die Namen der Teilnehmer angeben. Bitte beachtet, dass die Anzahl der Plätze begrenzt ist und die Teamgröße mindestens 5 Teilnehmer betragen muss. Die Anmeldefrist endet am <span className="text-decoration-underline">01. Juli um 18:30 Uhr</span>.
-            <br />
-            An den weiteren drei Tagen (vom 02. bis 04. Juli) stehen dann Blasmusik, Alpenrock und aktuelle Hits mit der Stimmungskapelle &quot;Frankenland Musikanten&quot; auf unserem Programm.<br />
-            Wie gewohnt bieten wir an allen Tagen Spiel, Spaß und Fahrgeschäfte für unsere kleinen und großen Gäste.<br />
-            Mehrere spannende Stationen zu &quot;Spiel und Spaß&quot;, neben dem großen Vergnügungspark, erwarten unsere kleinen Gäste am Samstagnachmittag ab 14:00 Uhr.<br />
-            Wir freuen uns auf euch!
-        </p>
-        <div className="d-flex flex-column align-items-center">
-            <div className="w-75">
-                <img className="img-fluid rounded" src="/img/news/fest2022/banner.png" alt="Banner Fest" />
+    return (
+        <>
+            <h1>Heimat- und Feuerwehrfest 2022</h1>
+            <h5 className="text-muted">01.-04.07.2022</h5>
+            <p>In diesem Jahr findet nach 2-j&#228;hriger Pause das traditionelle Heimatfest in Rennertehausen am ersten Juliwochenende vom 01. bis 04.07.2022</p>
+            <div className="row">
+                <div className="col-8">
+                    <p>Der Auftakt hierzu startet am Freitag, den 01.07.2022 mit einem 11 Meter Turnier. Die Anmeldung hierzu kann &#252;ber unsere <a className="text-decoration-none" href="https://www.facebook.com/feuerwehr.rennertehausen">Facebookseite</a> oder per Email an <a className="text-decoration-none" href="mailto://vorstand@feuerwehr-rennertehausen.de">vorstand@feuerwehr-rennertehausen.de</a> erfolgen. Dabei sollten ein Teamname sowie die Namen der 6 Spieler (5 Sch&#252;tzen und 1 Torwart) angegeben werden. Das Startgeld betr&#228;gt 20&#8364;.</p>
+                    <p>An den weiteren drei Tagen stehen dann Blasmusik, Alpenrock und aktuelle Hits mit der Stimmungskapelle "Frankenland Musikanten" auf unserem Programm.</p>
+                    <p>An allen Tagen bieten wir wie gewohnt Spiel, Spa&#223; und Fahrgesch&#228;fte f&#252;r unsere kleinen und gro&#223;en G&#228;ste.</p>
+                    <p>Mehrere spanndende Stationen zu "Spiel und Spa&#223;", neben dem gro&#223;en Vergn&#252;gungspark, erwarten unsere kleinen G&#228;ste am Samstagnachmittag ab 14:00 Uhr.</p>
+                    <p>Wir freuen uns auf euch!</p>
+                </div>
+                <div className="col-4">
+                    <img className="img-fluid" src="/img/news/fest2022/plakat.png" />
+                </div>
             </div>
-        </div>
-        </>);
+        </>
+    )
 }
