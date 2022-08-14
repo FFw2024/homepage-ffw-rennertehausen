9c240b052a7bb0d243134e960a3a05bc57e8336d
Philipp Geil <46970229+PhiGei2000@users.noreply.github.com>
2022-08-14 13:09:31 +0200

    Finish page "Aktuelles"

--- public/js/aktuelles.js
+++ public/js/aktuelles.js
@@ -1,51 +1,18 @@
 const searchParams = window.location.search;
 
-const urlBase = 'https://feuerwehr-rennertehausen.de/';
+const urlBase = document.head.baseURI;
 
 function getData() {
+  const init = {
+    method: 'GET',
+    headers: {Accept: 'application/json', Referrer: `${urlBase}aktuelles.html`},
+    mode: 'no-cors'
+  };
+
   return fetch(`${urlBase}data/news.json`, init).then(res => {
     if (!res.ok) {
       console.log(res);
-      // throw res.statusText;
-
-      // debug only
-      const data = [
-        {
-          title: 'Termine Sommer 2022',
-          id: '2022_04',
-          link: '/aktuelles/termineSommer2022',
-          description:
-              'Die Feuerwehren aus Haina und Dodenau haben uns zu ihren Festen eingeladen. Diesen Einladungen wollen wir gerne folgen.'
-        },
-        {
-          title: 'Feuerwehr- und Heimatfest 2022',
-          id: '2022_03',
-          link: '/aktuelles/fest2022',
-          icon: 'img/news/fest2022/banner.png',
-          description:
-              'In diesem Jahr ist es endlich wieder soweit! Das traditionelle Heimat- und Feuerwehrfest in Rennertehausen wird nach 3 Jahren mit Einschränkungen durch die Corona-Pandemie wieder stattfinden.'
-        },
-        {
-          title: 'Osterfeuer 2022',
-          id: '2022_02',
-          icon: 'img/news/2022_02/2022_02_01.png',
-          description:
-              'In diesem Jahr laden wir euch wieder herzlich zu unserem Osterfeuer ein.',
-          text:
-              'In diesem Jahr laden wir euch wieder herzlich zu unserem Osterfeuer ein. Wie beim letzten Mal findet das Osterfeuer vor dem DGH statt.<br />Für Essen, Trinken und Feuer werden wir sorgen.<br />Das Feuer wird bei Einbruch der Dunkelheit angezündet.<br />Wir freuen uns auf euch!<br /><br />INFO:<br />Um eine Belästigung der Anwohner, durch zu starke Rauchentwicklung, zu vermeiden verbrennen wir ausschließlich trockenes Scheitholz. Eine Anlieferung von Strauchschnitt etc. ist daher leider nicht möglich.<br />Wir freuen uns, dass wir endlich wieder mit euch feiern können!'
-        },
-        {
-          title: 'Einrichtung einer Unterkunft für Geflüchtete',
-          id: '2022_01',
-          icon: 'img/news/2022_01/2022_01_01.png',
-          description:
-              'Am Samstag, den 19.03.2022 haben wir zusammen mit den Feuerwehren der Gemeinde Allendorf zusammen eine Unterkunft für Geflüchtete aus der Ukraine eingerichtet.',
-          text:
-              'Am Samstag, den 19.03.2022 haben wir zusammen mit den Feuerwehren der Gemeinde Allendorf zusammen eine Unterkunft für Geflüchtete aus der Ukraine eingerichtet.<br />In dieser Unterkunft können bis zu 300 Geflüchtete untergebracht werden.'
-        }
-      ];
-
-      return data;
+      throw res.statusText;
     }
 
     return res.json();
@@ -56,26 +23,46 @@ if (searchParams) {
   const entries = searchParams.substring(1).split('&');
   var values = new Map();
 
-  for (const entry in entries) {
+  entries.forEach(entry => {
     const parts = entry.split('=');
     values.set(parts[0], parts[1]);
-  }
+  });
 
   if (values.has('id')) {
-    getData().then(
-        data => {
+    const searchId = values.get('id');
 
+    getData()
+        .then(data => data.find(item => item.id == searchId))
+        .then(item => {
+          document.getElementById('title').innerText = item.title;
+          document.getElementById('newsList').className += ' invisible';
+
+          const contentElement = document.getElementById('content');
+          contentElement.className = '';
+
+          const year = item.id.substring(0, 4);
+
+          fetch(`${urlBase}aktuelles/${year}/${item.id}.html`, {
+            method: 'GET',
+            headers: {
+              Accept: 'text/html',
+            },
+            mode: 'no-cors'
+          }).then(res => {
+            if (!res.ok) {
+              console.log(res);
+              throw res.statusText;
+            }
+
+            return res.text();
+          }).then(html => contentElement.innerHTML = html);
         });
   }
 
 } else {
-  const init = {
-    method: 'GET',
-    headers: {Accept: 'application/json', Referrer: `${urlBase}aktuelles.html`},
-    mode: 'no-cors'
-  };
-
   function getCard(news) {
+    const link = `${urlBase}aktuelles.html?id=${news.id}`;
+
     let result = `
       <div class="col">
         <div class="card mb-3">
@@ -90,7 +77,8 @@ if (searchParams) {
               <div class="d-flex flex-column h-100">
                 <h5 class="card-title">${news.title}</h5>
                 <p class="card-text overflow-hidden">${news.description}</p>
-                <a class="card-link mt-auto align-self-end">weiterlesen</a>
+                <a class="card-link mt-auto align-self-end" href="${
+        link}">weiterlesen</a>
               </div>  
             </div>
           </div>
@@ -101,8 +89,7 @@ if (searchParams) {
     return result;
   }
 
-
-  .then(createList);
+  getData().then(createList);
 
   function createList(json) {
     let newsList = document.getElementById('newsList');
