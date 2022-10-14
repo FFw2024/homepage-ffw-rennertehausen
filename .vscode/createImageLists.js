3c397a0c58a59764e5376588bce73645901fe194
Philipp Geil <Philipp-Geil@outlook.com>
2022-10-14 17:01:19 +0200

    add image hashs

--- .vscode/createImageLists.js
+++ .vscode/createImageLists.js
@@ -1,9 +1,19 @@
 #!/usr/bin/node
 const fs = require('fs')
+const crypto = require('crypto');
+
+const hash = crypto.createHash('sha256');
 
 const imageDirs = [
   `${__dirname}/../public/img/alarms`, `${__dirname}/../public/img/exercises`
 ];
+
+function computeHash(filename) {
+  var imgData = fs.readFileSync(filename);
+
+  hash.update(imgData)
+  return hash.copy().digest('hex');
+}
 
 function createImageList(directory) {
   const filenames = fs.readdirSync(directory, {withFileTypes: true});
@@ -11,7 +21,7 @@ function createImageList(directory) {
   const imageFiles =
       filenames.filter((file) => file.isFile() && file.name.endsWith('.jpeg'))
           .map(image => {
-            return {filename: image.name};
+            return {filename: image.name, hash: computeHash(`${directory}/${image.name}`)};
           });
 
   if (imageFiles.length > 0) {
