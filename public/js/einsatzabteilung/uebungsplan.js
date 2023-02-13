81ded6b20210faa181dc4590d7abd4ae598e4fbb
PhiGei2000 <Philipp-Geil@outlook.com>
2023-02-13 19:18:34 +0100

    Add page "Übungsplan"

---
+++ public/js/einsatzabteilung/uebungsplan.js
@@ -0,0 +1,41 @@
+const table = document.getElementById("tableBody")
+
+function createRow(date, time, desc) {
+    const row = document.createElement('tr');
+
+    const dateCol = document.createElement('td');
+    dateCol.innerText = date;
+    row.appendChild(dateCol);
+
+    const timeCol = document.createElement('td');
+    timeCol.innerText = time;
+    row.appendChild(timeCol);
+
+    const descCol = document.createElement('td');
+    descCol.innerText = desc;
+    row.appendChild(descCol);
+
+    table.appendChild(row);
+}
+
+const dateFormatOptions = {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric"
+};
+
+const timeFormatOptions = {
+    hour: "2-digit",
+    minute: "2-digit"
+};
+
+fetch('data/nextExercises.json')
+    .then(async (res) => {
+        const data = await res.json();
+
+        data.forEach((item) => {
+            const date = new Date(item.date);
+
+            createRow(date.toLocaleDateString('de-de', dateFormatOptions), date.toLocaleTimeString('de-de', timeFormatOptions) + " Uhr", item.desc)
+        });
+    });
