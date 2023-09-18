1ad0006ad8e2f096531402b22044b0f225227b62
PhiGei2000 <Philipp-Geil@outlook.com>
2023-09-18 13:41:04 +0200

    Update files and rename workflow file

--- public/js/einsatzabteilung/uebungsplan.js
+++ public/js/einsatzabteilung/uebungsplan.js
@@ -1,7 +1,7 @@
 const table = document.getElementById("tableBody")
 
 function createRow(date, time, desc) {
-    const row = document.createElement('tr');    
+    const row = document.createElement('tr');
 
     const dateCol = document.createElement('td');
     dateCol.innerText = date;
@@ -36,6 +36,6 @@ fetch('data/nextExercises.json')
         data.filter((item) => new Date(item.date) > Date.now()).forEach((item) => {
             const date = new Date(item.date);
 
-            createRow(date.toLocaleDateString('de-de', dateFormatOptions), date.toLocaleTimeString('de-de', timeFormatOptions) + " Uhr", item.desc, )
+            createRow(date.toLocaleDateString('de-de', dateFormatOptions), date.toLocaleTimeString('de-de', timeFormatOptions) + " Uhr", item.desc)
         });
     });
